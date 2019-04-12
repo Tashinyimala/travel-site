c0ca37d6fc2fbf52f9835e3614e63be12126757a
Tashi Nyima <tashinyima@hotmail.com>
2019-04-12 13:39:43 +0100

    styling header-section completed

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const cssvar = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const autoprefixer = require('autoprefixer');
 const mixins = require('postcss-mixins');
+const hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', () => {
   return gulp
     .src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, mixins, cssvar, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvar, nested, hexrgba, autoprefixer]))
     .on('error', errorInfo => {
       console.log(errorInfo.toString());
       this.emit('end');
