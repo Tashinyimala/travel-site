ebb11a6743e9214feb2815296d7dc00214a15ab2
Tashi Nyima <tashinyima@hotmail.com>
2019-04-14 16:42:38 +0100

    modal overlay completed

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+class Modal {
+  constructor() {
+    this.openModalButton = $('.open-modal');
+    this.modal = $('.modal');
+    this.closeModalButton = $('.modal__close');
+    this.events();
+  }
+
+  events() {
+    //clicking modal button
+    this.openModalButton.click(this.openModal.bind(this));
+
+    // clicking the x close modal button
+    this.closeModalButton.click(this.closeModal.bind(this));
+
+    // pushes any key
+    $(document).keyup(this.keyPressedHandler.bind(this));
+  }
+
+  keyPressedHandler(key) {
+    //Escape Keycode 27
+    if (key.keyCode == 27) {
+      this.closeModal();
+    }
+  }
+
+  openModal() {
+    this.modal.addClass('modal--is-visible');
+    return false;
+  }
+
+  closeModal() {
+    this.modal.removeClass('modal--is-visible');
+  }
+}
+
+export default Modal;
