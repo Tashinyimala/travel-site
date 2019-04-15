f28637840c07f6b5893b9ff70268babc9fef419a
Tashi Nyima <tashinyima@hotmail.com>
2019-04-15 18:43:14 +0100

    brower-support completed

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const webpack = require('webpack');
 
-gulp.task('scripts', callback => {
+gulp.task('scripts', ['modernizer'], callback => {
   webpack(require('../../webpack.config.js'), (error, stats) => {
     if (error) {
       console.log(error.toString());
