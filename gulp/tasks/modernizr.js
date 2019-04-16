5196738ffdf921f9c5cb476de7e0b3880615ca45
Tashi Nyima <tashinyima@hotmail.com>
2019-04-16 15:10:33 +0100

    change dist to docs folder name

--- gulp/tasks/modernizr.js
+++ gulp/tasks/modernizr.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp'),
-modernizr = require('gulp-modernizr');
+const gulp = require('gulp');
+const modernizr = require('gulp-modernizr');
 
-gulp.task('modernizr', function() {
-  return gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
-    .pipe(modernizr({
-      "options": [
-        "setClasses"
-      ]
-    }))
+gulp.task('modernizr', () => {
+  return gulp
+    .src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
+    .pipe(
+      modernizr({
+        options: ['setClasses']
+      })
+    )
     .pipe(gulp.dest('./app/temp/scripts/'));
 });
